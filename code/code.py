906439a39750e03d3e3e57364b8c7c67b4073cf5
Lauro Gama <lauro.gama@fpf.br>
2015-07-16 13:49:16 -0400

    added pika and rabbitmq to the article

--- code/code.py
+++ code/code.py
@@ -1,9 +1,14 @@
+import logging
+
 from flask import Flask
 import pika
 from pika.exceptions import AMQPConnectionError
 
 app = Flask(__name__)
 QUEUES = ['sensors']
+EXCHANGES = [{"name": 'logs', "type": 'fanout'}]
+
+logging.basicConfig()
 
 
 @app.route('/')
@@ -11,7 +16,7 @@ def hello_world():
     return 'Hello World!'
 
 
-def create_queues(queues):
+def create_queues(channel, queues):
     for new_queue in queues:
         channel.queue_declare(queue=new_queue)
         channel.basic_consume(queue_callback,
@@ -23,14 +28,24 @@ def queue_callback(ch, method, properties, body):
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
+def declare_exchanges(channel, exchanges):
+    with channel.queue_declare(exclusive=True) as result:
+        for exchange in exchanges:
+            channel.exchange_declare(exchange=exchange.name,
+                                     type=exchange.type)
+            channel.queue_bind(exchange=exchange.name,
+                               queue=result.method.queue)
+        channel.basic_consume(queue_callback, queue=result.method.queue,
+                              no_ack=True)
+        channel.start_consuming()
+
+
 def connect_to_rabbitmq():
-    global channel
     try:
         connection = pika.BlockingConnection(pika.ConnectionParameters(
             'localhost'))
         channel = connection.channel()
-        create_queues(QUEUES)
-        channel.start_consuming()
+        declare_exchanges(channel, EXCHANGES)
         return True
     except AMQPConnectionError:
         print("Couldn't connect to RabbitMQ Broker")
