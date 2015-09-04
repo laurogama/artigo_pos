de73b4b62bdbb00e02664de3340ce2a4b0566e76
Lauro Gama <lauro.gama@fpf.br>
2015-09-04 11:33:43 -0400

    adequating article to chilecon format template

--- code/environ_wsn_data_analysis/broker_data_analysis.py
+++ code/environ_wsn_data_analysis/broker_data_analysis.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import matplotlib
+
 matplotlib.use('Qt4Agg')
 import matplotlib.pylab as plt
 from sqlalchemy import create_engine
-
 
 __author__ = 'laurogama'
 
@@ -14,9 +14,8 @@ def load_data():
     query = "SELECT * from message"
     return pd.read_sql_table("message", engine,
                              columns=['id', 'timestamp', 'temperature',
-                                      'humidity', 'sender', 'luminance'],
-                             parse_dates={
-                                 "timestamp": "YYYY-MM-DD HH:MM:SS.SSS"})
+                                      'humidity', 'luminance', 'gas'],
+                             parse_dates=True)
 
 
 # parse_dates={"timestamp": "YYYY-MM-DD HH:MM:SS.SSS"}
@@ -26,16 +25,18 @@ def visualize_data(df):
     # df = df[['id', 'timestamp', 'temperature', 'humidity', 'sender']]
     print df.dtypes
     print df.head()
-    # print df.tail()
-    # sns.distplot(df['temperature'])
-    # sns.pairplot(data=df, vars=['temperature', 'humidity'])
-    # sns.plt.show()
+    print df.describe()
+    plot_graphics(df)
+
+
+def plot_graphics(df):
+    # df.sort(columns='timestamp')
     df.plot(x='timestamp', y='temperature')
-    plt.show()
     df.plot(x='timestamp', y='humidity')
-    plt.show()
     df.plot(x='timestamp', y='luminance')
+    df.plot(x='timestamp', y='gas')
     plt.show()
+
 
 if __name__ == '__main__':
     engine = create_engine(SQL_ENGINE)
